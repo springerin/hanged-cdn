837e68e35a2fcb7802c9cbdbb40eea48cd2f8f4f
Lorena Ramonda <lorena.ramonda@gmail.com>
2020-10-10 16:43:04 +0200

    Add Vue

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 /*global Vue*/
 
-var app = '';
+var app = new Vue({
+  el: '#app',
+  data: {
+    message: 'Hello Vue!'
+  }
+});
 
 // this console log it's for debugging, it will be removed later on
 console.log(app);
