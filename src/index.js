808c6daba800afa26a960f8ee8e53a697ce2772c
Lorena Ramonda <lorena.ramonda@gmail.com>
2020-10-11 15:05:55 +0200

    Add computed property

--- src/index.js
+++ src/index.js
@@ -4,7 +4,12 @@ var app = new Vue({
   el: '#app',
   data: {
     message: 'Hello Vue!'
-  }
+  },
+  computed: {
+    messageReverse() {
+      return this.message.split('').reverse().join('')
+    }
+  },
 });
 
 // this console log it's for debugging, it will be removed later on
