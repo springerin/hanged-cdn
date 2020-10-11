2d5c3b4e233eaefbfe9d8a7d45e943918357e2aa
Lorena Ramonda <lorena.ramonda@gmail.com>
2020-10-11 12:38:04 +0200

    Add game html template

--- src/index.js
+++ src/index.js
@@ -4,3 +4,6 @@ var app = '';
 
 // this console log it's for debugging, it will be removed later on
 console.log(app);
+
+// skip parcel requireJs scope, expose the app to the world!
+window.app = app;
