91cb51ee774f117da6e12857caee8ec0eecabd33
Lorena Ramonda <lorena.ramonda@gmail.com>
2020-10-11 15:06:36 +0200

    Export app as global var

--- src/index.js
+++ src/index.js
@@ -9,3 +9,6 @@ var app = new Vue({
 
 // this console log it's for debugging, it will be removed later on
 console.log(app);
+
+// skip parcel requireJs scope, expose the app to the world!
+window.app = app;
