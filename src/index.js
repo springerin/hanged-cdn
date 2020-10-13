ef3b1c8c3869985a41db843f8ed0b954fb97c58a
Lorena Ramonda <lorena.ramonda@gmail.com>
2020-10-13 20:25:06 +0200

    Add method

--- src/index.js
+++ src/index.js
@@ -5,8 +5,21 @@ var app = new Vue({
     coins: '0 monete',
     attempsMessage: 'Indovina la parola entro <strong>10 tentativi</strong>',
     word2discover: 'meraviglioso',
-    letter2check: ''
-  }
+    letter2check: '',
+    goodLetters: [],
+    badLetters: [],
+  },
+  methods: {
+    confirm() {
+      const letter = this.letter2check.toLowerCase()
+      if (this.word2discover.includes(letter)) {
+        if (!this.goodLetters.includes(letter)) this.goodLetters.push(letter)
+      } else {
+        this.badLetters.push(letter)
+      }
+      this.letter2check = ''
+    }
+  },
 }).$mount('#app');
 
 
